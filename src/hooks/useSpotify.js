5772ee2198dd3329324ac99a409dd112968d88c1
Jmurdocks <jason.murdock@simplevms.com>
2026-09-12 12:59:03 -0400

    Add user-read-playback-state scope for device listing

--- src/hooks/useSpotify.js
+++ src/hooks/useSpotify.js
@@ -216,6 +216,7 @@ export function useSpotify(clientId) {
         'streaming',
         'user-read-email',
         'user-read-private',
+        'user-read-playback-state',
         'user-modify-playback-state',
         'playlist-read-private',
         'playlist-read-collaborative',
